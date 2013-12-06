5f58ee149681ce4157b8670b26af44fc2ab414d6
zhxhdean@gmail.com <zhxhdean@gmail.com>
2013-12-06 21:19:31 +0800

    print system time 	on ubuntu

--- wetrip/wetrip/views/home.py
+++ wetrip/wetrip/views/home.py
@@ -1,4 +1,5 @@
+#coding=utf8
 from django.shortcuts import render
-
+import time
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'time':time.strftime('%Y-%m-%d 星期%w %H:%M:%S ')})
