4313221c86edd489747b7bcb8e571b3e750bab88
zhxhdean <zhxhdean@gmail.com>
2013-12-06 16:55:57 +0800

    add index page

--- wetrip/wetrip/settings.py
+++ wetrip/wetrip/settings.py
@@ -1,5 +1,5 @@
 # Django settings for wetrip project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
@@ -124,6 +125,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'wetrip',
 )
 
 # A sample logging configuration. The only tangible logging
--- wetrip/wetrip/urls.py
+++ wetrip/wetrip/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'wetrip.views.home', name='home'),
+    url(r'^$', 'wetrip.views.home.index', name='home'),
     # url(r'^wetrip/', include('wetrip.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
