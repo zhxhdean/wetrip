17d4fc9e50e77d415d6b83e4e686fdfc6ab35c8e
zhxhdean <zhxhdean@gmail.com>
2013-12-09 17:06:20 +0800

    pull server

--- wetrip/wetrip/views/home.py
+++ wetrip/wetrip/views/home.py
@@ -1,6 +1,6 @@
 #coding=utf8
 import datetime as dt,time,random
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.conf import settings
 from wetrip.views import upload
 def index(request):
@@ -13,9 +13,13 @@ def add_info(request):
         upload.create_dir(dir_path)
         file_name =  str(int(time.time())) + str(random.randint(0,1000))
         note = request.POST['tt_notes']
+        if not note:
+            return render(request,'index.html',{'msg':'error'})
         try:
             with open(settings.MEDIA_ROOT + dir_path + '/' + file_name,'w') as f:
                 f.write(note)
             return render(request,'index.html',{'msg':'success'})
         except IOError ,e:            
             return render(request,'index.html',{'msg':e.strerror})
+    else:
+        return redirect('/')
